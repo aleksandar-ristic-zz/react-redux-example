a88cfc14651abdc46c20c60bab0fc11d822b38a9
Aleksandar Ristić <aristic665@gmail.com>
2021-09-10 23:50:10 +0200

    added reducers

--- src/index.js
+++ src/index.js
@@ -3,8 +3,10 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 import { createStore } from 'redux'
+import allReducers from './reducers'
 
 // STORE ==> GLOBALIZED STATE
+const store = createStore(allReducers)
 
 // ACTION
 
